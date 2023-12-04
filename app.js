7d492d7cd2e1fd64bf40a547ea8e170a854eb642
xuan7678 <xuanxuan2980@gmail.com>
2023-12-03 22:45:56 -0800

    add user api

--- app.js
+++ app.js
@@ -5,14 +5,46 @@ import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./assignments/routes.js";
+import session from "express-session";
+import UserRoutes from "./users/routes.js";
 import "dotenv/config";
+import mongoose from "mongoose";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+
+process.on('uncaughtException', function (err) {
+    console.log('Caught exception: ' + err);
+});
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING);
 
 const app = express()
 app.use(express.json());
-app.use(cors({redentials: true,
-    origin: process.env.FRONTEND_URL}
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+    }
 ));
+
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+
+app.use(
+    session(sessionOptions)
+);
+
 app.use(express.json());
+UserRoutes(app);
 Lab5(app);
 Hello(app);
 ModuleRoutes(app);
